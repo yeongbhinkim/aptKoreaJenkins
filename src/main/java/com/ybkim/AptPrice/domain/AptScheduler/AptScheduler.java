a05f65c501c48086a29b85c2e4358aae314b334e
yeongbhinkim <abc282v@naver.com>
2024-09-30 14:28:18 +0900

    깃 훅 젠킨스 테스트

--- src/main/java/com/ybkim/AptPrice/domain/AptScheduler/AptScheduler.java
+++ src/main/java/com/ybkim/AptPrice/domain/AptScheduler/AptScheduler.java
@@ -206,7 +206,7 @@ public class AptScheduler {
     }
 
 //    @Scheduled(cron = "0 10 20 * * ?")
-    //test
+
 
     //    @Scheduled(cron = "0 0 0 1 * ?")
 //    @Scheduled(cron = "0 12 18 * * ?")
